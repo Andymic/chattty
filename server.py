55d0e55fec8b7f46919d043bce765feb168e45bf
Andy Michel <Andy.Michel@lmco.com>
2016-06-07 17:22:39 -0400

    initial commit

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 #!chattty/bin/python 
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, request, jsonify
 from flask.ext.socketio import SocketIO, emit, join_room
 import eventlet
 
@@ -9,8 +9,27 @@ app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'super_secret'
 socketio = SocketIO(app)
- 
- 
+rooms = [
+	{
+        'id': 0,
+        'title': u'default'
+    },
+    {
+        'id': 1,
+        'title': u'ser'
+    },
+    {
+        'id': 2,
+        'title': u'jaks'
+       
+    },
+    {
+        'id': 3,
+        'title': u'tesSS'
+       
+    }
+]
+
 @app.route('/')
 def chat():
     return render_template('chat.html')
@@ -18,16 +37,37 @@ def chat():
 @app.route('/login')
 def login():
     return render_template('login.html')
- 
- 
+
+@app.route('/rooms')
+def get_rooms():
+	return jsonify({'rooms' : rooms})
+
+@socketio.on('join')
+def on_join(data):
+	username = data['username']
+	room = data['room']
+	join_room(room)
+	send(username + ' has entered the room.', room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+	username = data['username']
+	room = data['room']
+	leave_room(room)
+	send(username + ' has left the room.', room=room)
+
 @socketio.on('message', namespace='/chat')
 def chat_message(message):
 	emit('message', {'data': message['data']}, broadcast = True)
  
-@socketio.on('connect', namespace='/chat')
-def test_connect():
-    emit('my response', {'data': 'Connected', 'count': 0})
+@socketio.on('connected', namespace='/chat')
+def connected_user(message):
+    emit('addToUserList', {'data': message['data']}, broadcast = True)
+
+@socketio.on('disconnect', namespace='/chat')
+def connected_user(message):
+    emit('removeFromUserList', {'data': message['data']}, broadcast = True)
  
  
 if __name__ == '__main__':
-    socketio.run(app, host="", port=5000)
+    socketio.run(app, host="127.0.0.1", port=5000)
