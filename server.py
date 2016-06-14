c2104bf118e49859d27e16f32712b2a307cee28e
Andy Michel <Andy.Michel@lmco.com>
2016-06-14 18:05:11 -0400

    added history buffer, sending userupdatelist from server and chrome notification

--- server.py
+++ server.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'super_secret'
 socketio = SocketIO(app)
+users = []
+history_buff = []
 rooms = [
 	{
         'id': 0,
@@ -58,16 +60,36 @@ def on_leave(data):
 
 @socketio.on('message', namespace='/chat')
 def chat_message(message):
+	history_buff.append(message['data'])
+	print "history_buff"
+	print history_buff
 	emit('message', {'data': message['data']}, broadcast = True)
  
 @socketio.on('connected', namespace='/chat')
 def connected_user(message):
-    emit('addToUserList', {'data': message['data']}, broadcast = True)
+	user = message['data']['username']
 
-@socketio.on('disconnect', namespace='/chat')
-def connected_user(message):
-    emit('removeFromUserList', {'data': message['data']}, broadcast = True)
+	if not users or not user in users:
+		print "New user {0} added to list".format(user)
+		users.append(message['data']['username'])
+
+	if history_buff:
+		emit('message', {'data': history_buff}, broadcast = True)
+
+	print "Firing addToUserList event (connected_user)"
+	print users
+	emit('updateUserList', {'data': users}, broadcast = True)
+
+@socketio.on('disconnected', namespace='/chat')
+def disconnected_user(message):
+	user = message['data']['username']
+	if user in users:
+		users.remove(user)
+
+	print "Firing updateUserList event (disconnected_user)"
+	print users
+	emit('updateUserList', {'data': users}, broadcast = True)
  
  
 if __name__ == '__main__':
-    socketio.run(app, host="127.0.0.1", port=5000)
+    socketio.run(app, host="", port=5000)
